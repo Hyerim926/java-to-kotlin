3b7635a4a9fd0b880e467ee7bca22b61d9770843
Hyerim926 <ekfrhsk96@icloud.com>
2024-01-24 01:44:18 +0900

    feat: 함수

---
+++ src/main/kotlin/com/hey/lec08/Lec08Main.kt
@@ -0,0 +1,36 @@
+package com.hey.lec08
+
+fun main() {
+    printNameAndGender(name = "FEMALE", gender = "헤이")
+
+    printAll("A", "B", "C")
+    val array = arrayOf("A", "B", "C")
+    printAll(*array)
+}
+
+fun max(a: Int, b: Int) = if (a > b) a else b
+
+fun repeat(
+    str: String,
+    num: Int = 3,
+    useNewLine: Boolean = true
+) {
+    for (i in 1..num) {
+        if (useNewLine) {
+            println(str)
+        } else {
+            print(str)
+        }
+    }
+}
+
+fun printNameAndGender(name: String, gender: String) {
+    println(name)
+    println(gender)
+}
+
+fun printAll(vararg strings: String) {
+    for (str in strings) {
+        println(str)
+    }
+}
